568af7e579cfb9ed813915bf0270286bf687e712
Nathan <s3717205@student.rmit.edu.au>
2021-05-22 14:22:49 +1000

    Added printBoard function

---
+++ sudoku-solver.py
@@ -0,0 +1,34 @@
+board = [
+    [3, 0, 0, 6, 1, 0, 0, 0, 8],
+	[0, 0, 2, 0, 3, 0, 7, 6, 0],
+	[0, 0, 0, 7, 5, 0, 2, 9, 0],
+	[0, 9, 0, 8, 0, 0, 0, 1, 0],
+	[0, 4, 0, 1, 7, 3, 0, 5, 0],
+	[0, 5, 0, 0, 0, 9, 0, 2, 0],
+	[0, 3, 7, 0, 4, 1, 0, 0, 0],
+	[0, 2, 5, 0, 8, 0, 9, 0, 0],
+	[4, 0, 0, 0, 9, 7, 0, 0, 2]
+]
+
+def printBoard(board):
+    rows = len(board)
+    cols = len(board[0])
+
+    for i in range(rows):
+
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - -")
+
+        for j in range(cols):
+            
+            if j % 3 == 0 and j != 0:
+                print("| ", end = '')
+
+            if board[i][j] == 0:
+                print("0 ", end = '')
+            else:
+                print(str(board[i][j]) + " ", end = '')
+
+        print()
+
+printBoard(board)
